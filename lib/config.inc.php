2ed51116650094dce8bb15c778a5cf4cd26551e7
Damon Parker <damonp@siliconsys.com>
2014-09-23 12:04:17 -0500

    add emails to config

--- lib/config.inc.php
+++ lib/config.inc.php
@@ -9,7 +9,9 @@
     define('SBTCP_MIN_CONFIRMATIONS', 1);
     define('SBTCP_BLOCKCHAIN_SECRET', '');
     define('SBTCP_EXCH_RATE_REFRESH', 60);
-    define('SBTCP_CALLBACK_URL', '');
+    define('SBTCP_EMAIL_FROM', 'From Name <pay.bot@domain.com>');
+    define('SBTCP_EMAIL_ADMIN', 'Admin Name <admin@domain.com>');
+    define('SBTCP_CALLBACK_URL', 'http://domain.com/path/to/callback.php');
 
-    include('functions.inc.php');
+    include('helper.class.php');
     include('api.class.php');
