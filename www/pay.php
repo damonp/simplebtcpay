2492c3741a58169c2f0cbb8c10957a490897b09e
Damon Parker <damonp@siliconsys.com>
2014-10-04 14:04:07 -0500

    allow linking directly to saved order by passing $oid to form.php or pay.php

--- www/pay.php
+++ www/pay.php
@@ -1,106 +1,113 @@
 <?php
 
-    include_once('../app/lib/config.inc.php');
+   include_once('../app/lib/config.inc.php');
 
-    //- prefer _POSTed variables
-    if(count($_POST) > 0)   {
+   $tot_drk = $tot_usd = $oid = $odesc = $order = null;
+   //- prefer _POSTed variables
+   if(count($_POST) > 0)   {
       $filters = array(
-                       'tot_drk'  => FILTER_SANITIZE_STRING,
-                       'tot_usd'  => FILTER_SANITIZE_STRING,
-                       'oid'      => FILTER_SANITIZE_STRING,
-                       'odesc'    => FILTER_SANITIZE_STRING,
-                       'oemail'   => FILTER_SANITIZE_STRING,
-                       'act'      => FILTER_SANITIZE_STRING,
-                      );
+                     'tot_drk'  => FILTER_SANITIZE_STRING,
+                     'tot_usd'  => FILTER_SANITIZE_STRING,
+                     'oid'      => FILTER_SANITIZE_STRING,
+                     'odesc'    => FILTER_SANITIZE_STRING,
+                     'oemail'   => FILTER_SANITIZE_STRING,
+                     'act'      => FILTER_SANITIZE_STRING,
+                     );
       extract(filter_input_array(INPUT_POST, $filters));
-    }   elseif(count($_GET) > 0)   {
-    //- but accept in _GET if _POST empty
+   }   elseif(count($_GET) > 0)   {
+      //- but accept in _GET if _POST empty
       $filters = array(
-                       'tot_drk'  => FILTER_SANITIZE_STRING,
-                       'tot_usd'  => FILTER_SANITIZE_STRING,
-                       'oid'      => FILTER_SANITIZE_STRING,
-                       'odesc'    => FILTER_SANITIZE_STRING,
-                       'oemail'   => FILTER_SANITIZE_STRING,
-                       'act'      => FILTER_SANITIZE_STRING,
-                      );
+                     'tot_drk'  => FILTER_SANITIZE_STRING,
+                     'tot_usd'  => FILTER_SANITIZE_STRING,
+                     'oid'      => FILTER_SANITIZE_STRING,
+                     'odesc'    => FILTER_SANITIZE_STRING,
+                     'oemail'   => FILTER_SANITIZE_STRING,
+                     'act'      => FILTER_SANITIZE_STRING,
+                     );
       extract(filter_input_array(INPUT_GET, $filters));
-    }
+   }
 
-    include_once('app/lib/main.inc.php');
+   include_once('app/lib/main.inc.php');
 
-    if($tot_drk > 0)    {
-        $total = $tot_drk;
-        $tot_usd = round($tot_drk * $exch_rate, 2);
-    }   else    {
-        $tot_drk = round($tot_usd / $exch_rate, 8);
-        $total = $tot_drk;
-    }
+   $order = false;
+   if($_GET['oid'] != '')  {
+      $order = Helper::get_order($oid);
+      if($order)  {
+         $tot_drk = $order->tot_drk;
+         $tot_usd = $order->tot_usd;
+         $address = $order->address;
+         $odesc = $order->odesc;
+         $oemail = $order->oemail;
+         $secret = $order->secret;
+      }
+   }
 
-    if($total < 0.001)  {
+   if(!$order) {
+      $oid = Helper::rand_id();
+      $secret = Helper::rand_id(13);
+   }
+
+   if($tot_usd > 0)    {
+      $tot_drk = round($tot_usd / $exch_rate, 8);
+      $total = $tot_drk;
+   }   else    {
+      $tot_usd = round($tot_drk * $exch_rate, 2);
+      $total = $tot_drk;
+   }
+
+   if($total < 0.001)  {
       header('Location: ./form.php?act=error.minimum');
       return false;
-    }
+   }
 
-    if($oid == '')  {
-        //- need to allow existing check to prevent collisions
-        $oid = Helper::rand_id();
-    }
+   if(!$receive_addr = $api->get_receive_address(SBTCP_RECEIVE_ADDR, $secret, $oid))   {
+      $receive_addr = SBTCP_RECEIVE_ADDR;
+      error_log('Invalid Receive Address. Defaulting to main address. Disable at:');
+      error_log('['.__LINE__.'] : '.__FILE__);
+   }
 
-    $secret = Helper::rand_id(13);
+   //- harcode to main address if not using a forwarding address
+   //$receive_addr = SBTCP_RECEIVE_ADDR;
 
-    if(!$receive_addr = $api->get_receive_address(SBTCP_RECEIVE_ADDR, $secret, $oid))   {
-        $receive_addr = SBTCP_RECEIVE_ADDR;
-        error_log('Invalid Receive Address. Defaulting to main address. Disable at:');
-        error_log('['.__LINE__.'] : '.__FILE__);
-    }
+   try {
+      $sql =   "REPLACE INTO orders ".
+               "(oid, total, email, desc, status, btc_usd, ".
+               "tot_usd, tot_drk, address, secret, t_stamp) ".
+               "VALUES ".
+               "(:oid, :total, :email, :desc, :status, :btc_usd, ".
+               ":tot_usd, :tot_drk, :address, :secret, :t_stamp)";
 
-    //- harcode to main address if not using receive_address from blockchain
-    //$receive_addr = SBTCP_RECEIVE_ADDR;
+      $qry = $db->prepare($sql);
+      $vars = array(
+                  ':oid'    => $oid,
+                  ':total'  => round($total, 8),
+                  ':email'  => $oemail,
+                  ':desc'   => $odesc,
+                  ':status' => 'PENDING',
+                  ':btc_usd'=> round($exch_rate, 2),
+                  ':tot_usd'=> round($tot_usd, 2),
+                  ':tot_drk'=> round($tot_drk, 8),
+                  ':address'=> $receive_addr,
+                  ':secret' => $secret,
+                  ':t_stamp'=> time()
+                  );
 
-    if($oid)    {
-        //$order = Helper::get_order($oid);
-    }
+      error_log('vars: '. print_r($vars,true));
+      foreach($vars as $key => $val)  {
+         $qry->bindValue($key, $val);
+      }
 
+      $qry->execute();
 
-    try {
-        $sql =  "REPLACE INTO orders ".
-                "(oid, total, email, desc, status, btc_usd, ".
-                "tot_usd, tot_drk, address, secret, t_stamp) ".
-                "VALUES ".
-                "(:oid, :total, :email, :desc, :status, :btc_usd, ".
-                ":tot_usd, :tot_drk, :address, :secret, :t_stamp)";
-
-        $qry = $db->prepare($sql);
-        $vars = array(
-                      ':oid'    => $oid,
-                      ':total'  => round($total, 8),
-                      ':email'  => $oemail,
-                      ':desc'   => $odesc,
-                      ':status' => 'PENDING',
-                      ':btc_usd'=> round($exch_rate, 2),
-                      ':tot_usd'=> round($tot_usd, 2),
-                      ':tot_drk'=> round($tot_drk, 8),
-                      ':address'=> $receive_addr,
-                      ':secret' => $secret,
-                      ':t_stamp'=> time()
-                      );
-
-        error_log('vars: '. print_r($vars,true));
-        foreach($vars as $key => $val)  {
-          $qry->bindValue($key, $val);
-        }
-
-        $qry->execute();
-
-    }  catch (PDOException $e) {
-        error_log('error: '. print_r($e->getMessage(),true));
-        error_log('['.__LINE__.'] : '.__FILE__);
-        error_log('_REQUEST: '. print_r($_REQUEST,true));
-        error_log('vars: '. print_r($vars,true));
-    }
+   }  catch (PDOException $e) {
+      error_log('error: '. print_r($e->getMessage(),true));
+      error_log('['.__LINE__.'] : '.__FILE__);
+      error_log('_REQUEST: '. print_r($_REQUEST,true));
+      error_log('vars: '. print_r($vars,true));
+   }
 
 
-    include_once('app/lib/header.inc.php');
+   include_once('app/lib/header.inc.php');
 
 ?>
 
@@ -115,27 +122,27 @@
 
 <div id="invoice">
   <?php if($oid != ''): ?>
-    <div class="invrow">
-        <div class="invhead">ID:</div>
-        <div class="invitem"><?php echo $oid; ?></div>
-    </div>
+	 <div class="invrow">
+		  <div class="invhead">ID:</div>
+		  <div class="invitem"><?php echo $oid; ?></div>
+	 </div>
   <?php endif; ?>
   <?php if($odesc != ''): ?>
-    <div class="invrow">
-        <div class="invhead">Item:</div>
-        <div class="invitem"><?php echo $odesc; ?></div>
-    </div>
+	 <div class="invrow">
+		  <div class="invhead">Item:</div>
+		  <div class="invitem"><?php echo $odesc; ?></div>
+	 </div>
   <?php endif; ?>
   <?php if($oemail != ''): ?>
-    <div class="invrow">
-        <div class="invhead">Email:</div>
-        <div class="invitem"><?php echo $oemail; ?></div>
-    </div>
+	 <div class="invrow">
+		  <div class="invhead">Email:</div>
+		  <div class="invitem"><?php echo $oemail; ?></div>
+	 </div>
   <?php endif; ?>
-    <div class="invrow">
-        <div class="invhead">Total:</div>
-        <div class="invitem"><?php echo number_format($total, 8); ?> <?php echo SBTCP_COIN; ?></div>
-    </div>
+	 <div class="invrow">
+		  <div class="invhead">Total:</div>
+		  <div class="invitem"><?php echo number_format($total, 8); ?> <?php echo SBTCP_COIN; ?></div>
+	 </div>
 </div>
 <br />
 
@@ -160,21 +167,21 @@
 <!--
 
 $( "#checkreceipt" ).click(function() {
-    $("#results" ).show( "slow", function() {
-      $("#results").html('<div id="loading"><img src="images/loader.gif" alt="loading" height="20" width="20" align="center" /></div>');
-      $.ajax({
-        url: "ajax.php?act=check_receipt&addr=<?php echo $receive_addr;?>&oid=<?php echo $oid;?>",
-        cache: false,
-        success: function(html){
-          var json = $.parseJSON(html);
-          if(!json.return)  {
-            $("#results").html(json.message);
-          } else  {
-            $("#results").html('Received: '+json.balance+'<br />'+json.message);
-          }
-        }
-      });
-    });
+	 $("#results" ).show( "slow", function() {
+		$("#results").html('<div id="loading"><img src="images/loader.gif" alt="loading" height="20" width="20" align="center" /></div>');
+		$.ajax({
+		  url: "ajax.php?act=check_receipt&addr=<?php echo $receive_addr;?>&oid=<?php echo $oid;?>",
+		  cache: false,
+		  success: function(html){
+			 var json = $.parseJSON(html);
+			 if(!json.return)  {
+				$("#results").html(json.message);
+			 } else  {
+				$("#results").html('Received: '+json.balance+'<br />'+json.message);
+			 }
+		  }
+		});
+	 });
 });
 
 -->
